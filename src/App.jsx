d37a50305b0fd7e2b64fc812df5b7f40b1802ec9
Nazish1712 <nazish1712@gmail.com>
2026-03-16 17:46:03 +0530

    created a landing page for practice

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Hero from "./components/Hero"
+function App() {
+  return (
+    <div className="h-screen bg-neutral-900 flex justify-center">
+       <Hero></Hero>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+   return (
+    <div className="my-40 w-full flex flex-col items-center justify-center">
+      <h1 className=" text-center text-7xl font-bold tracking-tight max-w-2xl leading-tight bg-clip-text text-transparent bg-linear-to-b from-neutral-50 to-neutral-500   ">Unleash the power of intuitive finance</h1>
+      <p className="text-neutral-500 text-xl max-w-3xl text-center mt-10 selection:bg-white">Say goodbye to the outdated financial tools. Every small business owner, 
+      regardless of the background, can now manage their business like a pro.
+      Simple. Intuitive. And never boring.
+      </p>
+      <div className=" mt-8 flex justify-center">
+        <input type="text" className="rounded-xl border border-neutral-600 mr-4 text-white px-4 placeholder:text-neutral-500"
+        placeholder="Enter your email" />
+        <button className="relative border border-neutral-700 text-white px-4 py-2 rounded-xl cursor-pointer">
+        <div className="absolute -bottom-px inset-x-0 w-full h-px bg-gradient-to-r from-transparent via-sky-600 to-transparent-"></div>    
+            Join Waitlist</button>
+      </div>
+    </div>
+   )
+}
+
+export default Hero
